ae525b01d6d7dbf05c8f5114907467b07e856b0c
Chineek Favors <chineekfavors01@gmail.com>
2019-10-17 16:57:17 -0700

    added styles to componet WelcomeLogIn.js with styles.components

--- src/Components/WelcomeLogIn.js
+++ src/Components/WelcomeLogIn.js
@@ -1,5 +1,54 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import styled from 'styled-components';
+
+import Card from './Card.js';
+
+// styled components
+const NavOuterDiv = styled.nav`
+	 display: flex;
+  	flex-direction: row;
+  	font-size: 1.5rem;
+`;
+const DivOneLineADay = styled.div`
+	margin-top: 25px;
+  	margin-left: 3%;
+
+`;
+
+const DivOtherLinks = styled.div`
+	margin-left: 45%;
+  	width: 40%;
+`;
+
+const UlLinks = styled.div`
+	 display: flex;
+	  flex-direction: row;
+	  justify-content: space-between;
+	  list-style: none;
+	  width: 80%;
+	  margin-top: 25px;
+`;
+const H1Wrapper = styled.h1`
+	  margin-top: 200px;
+  		font-size: 2rem;
+`;
+
+const Button = styled.button`
+	font-size: 1.5rem;
+   	border-radius: 5px;
+    width: 75%;
+    color: rgb(218, 216, 250);
+    margin: 20px auto;
+    background-color: rgb(202, 200, 248);
+`;
+
+const Paragraph = styled.p`
+	font-size: 1.2rem;
+  	color: rgb(119, 136, 150);
+
+`;
 
 class WelcomeLogIn extends React.Component {
 	state = {
@@ -23,26 +72,58 @@ class WelcomeLogIn extends React.Component {
 		console.log(this.state);
 		return (
 			<>
-				<h1>Welcome back!</h1>
-				<form>
+				<Router>
+					<NavOuterDiv>
+						<DivOneLineADay>
+							<Link className='oneADaylink' to='/card'>One Line a Day </Link>
+							<Route  path='/Card' render={props => { 
+								return <Card />
+							}}/>
+						</DivOneLineADay>
 					
-					<input 
+						<DivOtherLinks>
+						<UlLinks>
+							<li>
+							<Link className='otherLinks' to='/about-us'>About Us</Link>
+							</li>
+							<li>
+							<Link className='otherLinks' to='/learn-more'>Learn more</Link>
+							</li>
+							<li>
+							<Link  className='otherLinks' to='/sign-up'>Sign Up</Link>
+							</li>
+						</UlLinks>
+
+						</DivOtherLinks>
+					</NavOuterDiv>
+			
+				
+				
+				<H1Wrapper>Welcome back!</H1Wrapper>
+				
+				<form className='form'>
+					<input className='inputsWelcomePage' 
 						type='email' 
 						name='email'
 						placeholder='Email'
 						value={this.state.email}
 						onChange={this.handleChanges}
 					/>
-					<input 
+					<input  className='inputsWelcomePage'
 						type='password' 
 						name='password'
 						placeholder='Password'
 						value={this.state.passwoed}
 						onChange={this.handleChanges}
 					/>
-					<button>Sign In</button>
-					<p>Don't have an account? Sign UP</p>
+					
+					<Button >Sign In</Button>
+					<Link className='paragraphLink' to='/sign-up'>
+						<Paragraph>Don't have an account? Sign Up</Paragraph>
+					</Link>
 				</form>
+				</Router>
+			
 			</>
 		);
 	}
---
+++ src/Components/card.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Card = () => {
+
+	return (
+		<h1> using card.js to test Navlinks and router correct</h1>
+	)
+}
+export default Card;
