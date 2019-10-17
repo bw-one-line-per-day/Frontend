3bf61c40f59f9a4fa468b55df2cd4fef32223c52
Chineek Favors <chineekfavors01@gmail.com>
2019-10-16 21:41:31 -0700

    added state for form input

--- src/Components/WelcomePage.js
+++ src/Components/WelcomePage.js
@@ -1,30 +1,57 @@
 import React from 'react';
 
 
-const WelcomePage = () => {
-	return (
-		<>
-			<h1>Welcome!</h1>
-			<form>
-				<input 
-					type='text' 
-					name='name'
-					placeholder='Name'
-				/>
-				<input 
-					type='email' 
-					name='email'
-					placeholder='Email'
-				/>
-				<input 
-					type='password' 
-					name='password'
-					placeholder='Password'
-				/>
-				<button>Sign Up</button>
-				<p>Already have an account? Sign In"sign in is a link"</p>
-			</form>
-		</>
-	);
+class WelcomePage extends React.Component {
+	state = {
+		name: '',
+		email: '',
+		password: ''
+	}
+
+
+	handleChanges = (e) => {
+		this.setState({
+			...this.state,
+			[e.target.name]: e.target.value
+			
+		});
+			
+
+	}
+
+
+	render(){
+		console.log(this.state);
+		return (
+			<>
+				<h1>Welcome!</h1>
+				<form>
+					<input 
+						type='text' 
+						name='name'
+						placeholder='Name'
+						value={this.state.name}
+						onChange={this.handleChanges}
+					/>
+					<input 
+						type='email' 
+						name='email'
+						placeholder='Email'
+						value={this.state.email}
+						onChange={this.handleChanges}
+					/>
+					<input 
+						type='password' 
+						name='password'
+						placeholder='Password'
+						value={this.state.passwoed}
+						onChange={this.handleChanges}
+					/>
+					<button>Sign Up</button>
+					<p>Already have an account? Sign In"sign in is a link"</p>
+				</form>
+			</>
+		);
+	}
 }
 export default WelcomePage;
