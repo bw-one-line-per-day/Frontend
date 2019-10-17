11963a09a3b1b2778be66cc506418a814eaea9f4
Chineek Favors <chineekfavors01@gmail.com>
2019-10-16 20:38:07 -0700

    created form with inputs for name, email, password and a button with text Sign Up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import WelcomPage from './Components/WelcomePage.js';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <WelcomPage />
        
     </div>
   );
--- src/Components/WelcomePage.js
+++ src/Components/WelcomePage.js
@@ -1,23 +1,30 @@
+import React from 'react';
+
 
 const WelcomePage = () => {
 	return (
-		<h1>Welcome!</h1>
-		<form>
-			<input 
-				type='text' 
-				name='name'
-			/>
-			<input 
-				type='email' 
-				name='email'
-			/>
-			<input 
-				type='password' 
-				name='password'
-			/>
-			<button>Sign Up</button>
-			<p>Already have an account? Sign In"sign in is a link"</p>
-		</form>
+		<>
+			<h1>Welcome!</h1>
+			<form>
+				<input 
+					type='text' 
+					name='name'
+					placeholder='Name'
+				/>
+				<input 
+					type='email' 
+					name='email'
+					placeholder='Email'
+				/>
+				<input 
+					type='password' 
+					name='password'
+					placeholder='Password'
+				/>
+				<button>Sign Up</button>
+				<p>Already have an account? Sign In"sign in is a link"</p>
+			</form>
+		</>
 	);
 }
 export default WelcomePage;
