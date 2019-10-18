78ed987281df572d8dbbde1a9b0f87b26b4f87ba
lworku <lydiasgthb@gmail.com>
2019-10-17 20:24:51 -0700

    setup EntryForm.js, added EntryForm.js to App.js, setup unpolished Route path for App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import HomePage from './Components/HomePage.js'; 
 import Nav from './Components/nav.js'; 
+import EntryForm from './Components/EntryForm.js';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <HomePage/>
+      <EntryForm/>
     </div>
   );
 }
--- src/Components/EntryForm.js
+++ src/Components/EntryForm.js
@@ -3,19 +3,19 @@ import React from 'react';
 function EntryForm() {
     return (
         <div className='EntryForm'>
-        <h1> One Line A Day </h1> 
-        <form>
-            <label>
-                <input type='text'/> 
-            </label>
+            <h1> One Line A Day </h1> 
+                <form>
+                    <label>
+                        <input type='text'/> 
+                    </label>
 
-            <label>
-                <input type='text'/> 
-            </label>
+                    <label>
+                        <input type='text'/> 
+                    </label>
 
-            <button>Save</button>
+                    <button>Save</button>
 
-        </form>
+                </form>
         </div>
     );
 }
