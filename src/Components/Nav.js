056a2d3634943db08749a28c06387714e83aae01
lauralyeinc <52268790+lauralyeinc@users.noreply.github.com>
2019-10-24 02:38:15 -0400

    Nav.js is done with styling and placement.

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,29 +1,51 @@
 import React from "react"; 
-import { Link } from 'react-router-dom'; 
 import styled from 'styled-components'; 
 import Logo from '../Images/Logo.png'; 
+import SignOutButton from '../Images/SignOutButton.png'; 
 
 function Nav() {
   return (
     <StyledNav>
-    <div className='Nav-Container'>
-      <img src={Logo} alt='note pad with pen' /> 
-      <h1> One Line a Day </h1> 
-      {/* <Link to='aboutus'> About Us </Link> */}
-      <div>
-      <button onClick={signOut}> Sign Out </button>
-      </div>
-      </StyledNav>
-    </div>
+      <LeftDiv>
+      <LogoStyled img src={Logo} alt='note pad with pen'/> 
+      <LogoTitle> One Line a Day </LogoTitle> 
+      </LeftDiv>
+      <Button> <ImgStyled img src={SignOutButton} alt={'Click here to sign out'} /> </Button>
+    </StyledNav>
   )
 }
 
-
+const LogoTitle = styled.h1`
+color: #e36f56;
+padding: .25rem; 
+`
 const StyledNav = styled.div`
 display: flex; 
-padding: 2.5rem;
+padding: .5rem;
 margin: 1rem; 
 background: #FEE5E2;
 `
 
+const ImgStyled = styled.img`
+height: 50px; 
+`
+const Button = styled.button`
+display: flex;
+flex-direction: column;
+margin: 2rem;
+border: none;
+background: none;
+`  
+
+const LogoStyled = styled.img`
+height: 75px; 
+padding: .5rem; 
+`
+const LeftDiv = styled.div`
+display: flex; 
+margin: auto;
+justify-content: left;
+align-content: left; 
+width: 80%;
+`
 export default Nav; 
