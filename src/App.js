2bf243cdff5c9623abf29011077ec069bcb132f2
maddiestrange <maddie.strange@gmail.com>
2019-10-24 21:48:03 -0500

    routing error fixed on App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     <Router>
 
       <div className="App">
-        <Route exact path="/" component={SignUp} />
+
         <Switch>
         <Route exact path="/" component={SignUp} />
         <Route exact path="/login" component={WelcomeLogIn} />
