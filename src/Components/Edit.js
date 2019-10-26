de67463e255fd54df025c9ad37e0424162039f76
lauralyeinc <52268790+lauralyeinc@users.noreply.github.com>
2019-10-25 23:58:10 -0400

    Adding the api to be able to edit and post it back into entry archive. having an error with in my form input on the title. defaultvalue.

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -1,28 +1,68 @@
-import React, { useEffect } from 'react'; 
+import React, { useEffect, useState, useContext } from 'react'; 
 import { Link } from 'react-router-dom'; 
+import { axiosWithAuth } from 'Auth/axiosWithAuth';
+import {StoreContext} from '../contextAPI/Context.js'; 
 
 const Edit = (props) => {
-  useEffect (() => {
-    const id = props.match.params.id; 
-  }
+
+const [entry, setEntry] = useState(); 
+const id = parseInt(props.match.params.id,10); 
+const { userInfo } = useContext(StoreContext);
+
+  useEffect(function(){
+    axiosWithAuth().get(`users/${userInfo.id}/posts`)
+      .then(response => {
+        console.log(response, "entry to be changed");
+        setEntry(response.data.find((event) => event.id === id ))
+      }) 
+      .catch(error => {
+        console.log(error, "can't edit entry"); 
+      })
+    }, []); 
+  console.log(entry, "entry to be edited");
   
-  )
+
+  useEffect(function(){
+    axiosWithAuth().put(`users/posts/${id}`, {title: entry.title, contents: entry.contents})
+    .then(response => {
+      console.log(response, "response of editing a entry"); 
+      setEntry(response.data); 
+    })
+    .catch(error => {
+      console.log(error, "editing fail");
+    })
+  }, []);
+  console.log(entry, "entry after editing"); 
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(entry.title,"editing title");
+    console.log(entry.contents, "editing contents");
+  };
+
 return (
   <div>
     <Link to='/MyEntries'> 
     <button > here</button>
     </Link>
-    <form>
-      <input type='text' defaultValue='test' 
-      // defaultValue={props.title} 
-      />
-      <textarea rows='10' cols='50'> 
-      testing 
-      {/* {props.contents} */}
-      </textarea> 
+    <form onSubmit={event => handleSubmit(event)} >   
+      <label >
+        <input type='text' defaultValue='title'>
+          {entry.title} 
+        </input> 
+      </label>
+      <label>
+        <textarea rows='10' cols='50'> 
+          {entry.contents}
+        </textarea> 
+      </label>
+      <label>
+        <button> Submit! </button>
+      </label>
+
     </form>
   </div>
-)
+  )
 } 
 
 export default Edit; 
